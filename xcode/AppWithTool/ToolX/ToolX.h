f2dce08701d4ff0729edbb6cdf072cd8c9304591
randymcmillan <randymcmillan@protonmail.com>
2026-05-24 07:53:59 -0400

    AppWithTool:refactor XTool

---
+++ xcode/AppWithTool/ToolX/ToolX.h
@@ -0,0 +1,13 @@
+//
+//  ToolX.h
+//  ToolX
+//
+//  Created by git on 5/24/26.
+//
+
+#ifndef ToolX_h
+#define ToolX_h
+
+void ToolXRun(int argc, const char *argv[]);
+
+#endif /* ToolX_h */
--- xcode/AppWithTool/ToolX/main.m
+++ xcode/AppWithTool/ToolX/main.m
@@ -5,29 +5,60 @@
 //  Created by git on 4/25/23.
 //
 
-// REF: https://developer.apple.com/documentation/xcode/embedding-a-helper-tool-in-a-sandboxed-app
+#import "ToolX.h"
+#import <TargetConditionals.h>
 
-
-#import <Foundation/Foundation.h>
+#if TARGET_OS_OSX && !TARGET_OS_IPHONE
 #import <Cocoa/Cocoa.h>
-#include "execv_bridge.h"
-#include "async.h/async/async.h"
+#import <Foundation/Foundation.h>
+#include "AppWithTool/execcl_bridge.h"
+#include "AppWithTool/execv_bridge.h"
+#include "AppWithTool/logargc.h"
+#include "AppWithTool/logargv.h"
 
-void report() {
-
-    printf("ToolX:report()\n");
-
+static void report(void) {
+    NSLog(@"ToolX:report()");
 }
 
-int main(int argc, const char * argv[]) {
+static void runScriptSharedSupport(NSString *scriptName) {
+    NSTask *task = [[NSTask alloc] init];
+    [task setLaunchPath:@"/bin/sh"];
+
+    NSString *newpath = [NSString stringWithFormat:@"%@/%@", [[NSBundle mainBundle] sharedSupportPath], scriptName];
+    NSLog(@"shell script path: %@", newpath);
+    [task setArguments:@[newpath]];
+
+    NSPipe *pipe = [NSPipe pipe];
+    [task setStandardOutput:pipe];
+
+    NSFileHandle *file = [pipe fileHandleForReading];
+
+    [task launch];
+
+    NSData *data = [file readDataToEndOfFile];
+    NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"script returned:\n%@", string);
+}
+#endif
+
+void ToolXRun(int argc, const char *argv[]) {
+#if TARGET_OS_OSX && !TARGET_OS_IPHONE
     @autoreleasepool {
-    report();
-// int execv_bridge(const char *path, char *const argv[]);
-    //execv_bridge(argv[0],(char *const *)argv);
-    //execv_bridge(&argc,(char *const *)argv);
+        report();
 
-// logargv(&argc, (char *const *)argv);
+        int *count = &argc;
+        NSLog(@"logargc(count)");
+        int returncount = logargc(count);
+        NSLog(@"returncount = logargc(count) = %d", returncount);
 
+        execv_bridge(argv[0], (char **)argv);
+        execcl_bridge(argc, (char **)argv);
+
+        runScriptSharedSupport(@"template.sh");
+        runScriptSharedSupport(@"Script.sh");
     }
-    return 0;
+#else
+    (void)argc;
+    (void)argv;
+#endif
 }
