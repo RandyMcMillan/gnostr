dc88a246a11e8235ed6032aeae2f7c416f859c34
randymcmillan <randymcmillan@protonmail.com>
2026-05-24 07:28:36 -0400

    AppWithTool

---
+++ xcode/AppWithTool/AppWithTool/ViewController.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.h
+//  AppWithTool
+//
+//  Created by git on 4/25/23.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include "execv_bridge.h"
+#include "execcl_bridge.h"
+#include "async.h/async/async.h"
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
---
+++ xcode/AppWithTool/ToolX/main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  ToolX
+//
+//  Created by git on 4/25/23.
+//
+
+// REF: https://developer.apple.com/documentation/xcode/embedding-a-helper-tool-in-a-sandboxed-app
+
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+#include "execv_bridge.h"
+#include "async.h/async/async.h"
+
+void report() {
+
+    printf("ToolX:report()\n");
+
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+    report();
+// int execv_bridge(const char *path, char *const argv[]);
+    //execv_bridge(argv[0],(char *const *)argv);
+    //execv_bridge(&argc,(char *const *)argv);
+
+// logargv(&argc, (char *const *)argv);
+
+    }
+    return 0;
+}
