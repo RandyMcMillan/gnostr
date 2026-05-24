d68f73b197bcf11bd42c7ebe19fd78ed8d70d651
randymcmillan <randymcmillan@protonmail.com>
2026-05-24 07:42:14 -0400

    AppWithTool:macos building

--- xcode/AppWithTool/AppWithTool/AppDelegate.h
+++ xcode/AppWithTool/AppWithTool/AppDelegate.h
@@ -10,9 +10,11 @@
 #if TARGET_OS_OSX && !TARGET_OS_IPHONE
 #import <Cocoa/Cocoa.h>
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+@property (strong) NSWindow *window;
 #else
 #import <UIKit/UIKit.h>
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
 #endif
 
 
--- xcode/AppWithTool/AppWithTool/AppDelegate.m
+++ xcode/AppWithTool/AppWithTool/AppDelegate.m
@@ -6,23 +6,34 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 // #include "execv_bridge.h"
-
-@interface AppDelegate ()
-
-
-@end
 
 @implementation AppDelegate
 
+#if TARGET_OS_OSX && !TARGET_OS_IPHONE
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    self.window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 900, 640)
+                                              styleMask:(NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable)
+                                                backing:NSBackingStoreBuffered
+                                                  defer:NO];
+    self.window.title = @"AppWithTool";
+    self.window.contentViewController = [[ViewController alloc] init];
+    [self.window makeKeyAndOrderFront:nil];
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
+#else
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = [[ViewController alloc] init];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+#endif
 
 
 @end
