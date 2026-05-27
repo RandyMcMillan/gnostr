8f2a803ec9951a327143b162f257ddfdb0b07f2f
randymcmillan <randymcmillan@protonmail.com>
2026-05-27 15:53:43 -0400

    xcode/swift-universal/swiftyapp/Shared/Utilities/Universal_AppApp.swift

--- xcode/swift-universal/swiftyapp/Shared/Utilities/Universal_AppApp.swift
+++ xcode/swift-universal/swiftyapp/Shared/Utilities/Universal_AppApp.swift
@@ -5,30 +5,45 @@
 //  Created by Can Balkaya on 12/10/20.
 //
 
+import Darwin
 import RustyLib
 import SwiftUI
 
 @main
 struct Universal_AppApp: App {
+    @StateObject private var appState = AppState()
+
     var body: some Scene {
         WindowGroup {
             AppLifecycleView()
+                .environmentObject(appState)
         }
     }
 }
 
+final class AppState: ObservableObject {
+    @Published var privateKey: String = ""
+}
+
 private struct AppLifecycleView: View {
+    @EnvironmentObject private var appState: AppState
     @Environment(\.scenePhase) private var scenePhase
     @State private var didStartNetwork = false
 
     var body: some View {
         MainView()
             .onAppear {
+                applyPrivateKeyEnvironment()
                 startNetworkIfNeeded()
+            }
+            .onChange(of: appState.privateKey) { _ in
+                applyPrivateKeyEnvironment()
+                restartNetworkIfNeeded()
             }
             .onChange(of: scenePhase) { phase in
                 switch phase {
                 case .active:
+                    applyPrivateKeyEnvironment()
                     startNetworkIfNeeded()
                 case .background:
                     stopNetwork()
@@ -46,8 +61,25 @@ private struct AppLifecycleView: View {
         _ = p2pNetworkStart()
     }
 
+    private func restartNetworkIfNeeded() {
+        guard didStartNetwork else { return }
+        _ = p2pNetworkStop()
+        didStartNetwork = false
+        startNetworkIfNeeded()
+    }
+
     private func stopNetwork() {
         _ = p2pNetworkStop()
         didStartNetwork = false
+    }
+
+    private func applyPrivateKeyEnvironment() {
+        if appState.privateKey.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            unsetenv("GNOSTR_NSEC")
+        } else {
+            appState.privateKey.withCString { pointer in
+                setenv("GNOSTR_NSEC", pointer, 1)
+            }
+        }
     }
 }
