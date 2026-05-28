5a39e4f1bfc4b111ff431f7dc039aa66462103b1
randymcmillan <randymcmillan@protonmail.com>
2026-05-28 13:59:59 -0400

    ngit/tests/git_remote_nostr/main.rs

--- ngit/tests/git_remote_nostr/main.rs
+++ ngit/tests/git_remote_nostr/main.rs
@@ -4,7 +4,7 @@ use anyhow::{Context, Result};
 use futures::join;
 use nostr::nips::{nip01::Coordinate, nip19::Nip19Coordinate};
 use nostr_sdk::{Event, JsonUtil, Kind, RelayUrl, ToBech32, secp256k1::rand};
-use gnostr_asyncgit::git2::Oid;
+use git2::Oid;
 use relay::Relay;
 use serial_test::serial;
 use test_utils::{git::GitTestRepo, *};
