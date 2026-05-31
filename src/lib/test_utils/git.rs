cf2907c5c6657a56edf73e3c43a9f057148f01e6
randymcmillan <randymcmillan@protonmail.com>
2026-05-31 11:10:48 -0400

    src/lib/test_utils/git.rs

--- src/lib/test_utils/git.rs
+++ src/lib/test_utils/git.rs
@@ -364,15 +364,12 @@ mod tests {
     fn test_git_test_repo_new() -> Result<()> {
         let repo_main = GitTestRepo::new("main")?;
         assert!(repo_main.dir.exists());
-        assert_eq!(repo_main.git_repo.head()?.shorthand(), Some("main"));
+        assert_eq!(repo_main.git_repo.head()?.shorthand()?, "main");
         assert_eq!(repo_main.get_checked_out_branch_name()?, "main");
 
         let repo_dev = GitTestRepo::new("development")?;
         assert!(repo_dev.dir.exists());
-        assert_eq!(
-            repo_dev.git_repo.head()?.shorthand(),
-            Some("development")
-        );
+        assert_eq!(repo_dev.git_repo.head()?.shorthand()?, "development");
         assert_eq!(repo_dev.get_checked_out_branch_name()?, "development");
 
         Ok(())
