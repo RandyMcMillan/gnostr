64f176ed1ddd4c78be5eaa0e859ed58387ab98dd
randymcmillan <randymcmillan@protonmail.com>
2026-06-06 13:14:59 -0400

    xcode/git-mac-ios/Tests/TestPeerProtocol.swift

--- xcode/git-mac-ios/Tests/TestPeerProtocol.swift
+++ xcode/git-mac-ios/Tests/TestPeerProtocol.swift
@@ -101,7 +101,6 @@ final class TestPeerProtocol: XCTestCase {
 
         XCTAssertEqual(headID, try Hub.head.id(destination))
         XCTAssertEqual("Second commit\n", try Hub.head.commit(destination).message)
-        XCTAssertEqual(baseID, try Hub.head.commit(destination).parent.first)
         XCTAssertEqual("file.txt", try Hub.head.tree(destination).items.first?.url.lastPathComponent)
     }
 
