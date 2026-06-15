d0efe8e2bbd4849cea0afb999e94e959dd28ea34
randymcmillan <randymcmillan@protonmail.com>
2026-06-15 19:02:31 -0400

    p2p/examples/fractal_swarm_perfect_ip.rs

--- p2p/examples/fractal_swarm_perfect_ip.rs
+++ p2p/examples/fractal_swarm_perfect_ip.rs
@@ -10,6 +10,7 @@ use gnostr_p2p::perfect_ip::{
     build_fractal_swarm, generate_manifest, packetize, summarize_packets, FractalBehaviourEvent,
     IntegrityManager, ProtocolSlice,
 };
+use gnostr_p2p::message::{EventBuilder, PrivateKey};
 use libp2p::{request_response, swarm::SwarmEvent};
 use sha2::{Digest, Sha256};
 
